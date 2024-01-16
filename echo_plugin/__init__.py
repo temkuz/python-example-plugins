8505fd6a5a71b21dad42dedd2953ffa790e5c4ee
temkuz <temkuz@yandex.ru>
2024-01-16 19:32:14 +0500

    feat(config_cli)!: opting out of the mandatory 'config_cli'

--- echo_plugin/__init__.py
+++ echo_plugin/__init__.py
@@ -1,15 +1,12 @@
-from argparse import ArgumentParser
-
-from poc_plugin.types.plugins import CLIUtils
+from poc_plugin.variables.cli import CLI_SUBPARSER
 from .functions import show_message
 
 
-def config_cli(cli_utils: CLIUtils):
-    sub = cli_utils.subparser
-    echo_parser: ArgumentParser = sub.add_parser('echo', help='echo plugin command')
+def config_cli():
+    echo_parser = CLI_SUBPARSER.add_parser('echo', help='echo plugin command')
     echo_parser.set_defaults(func=show_message)
     echo_parser.add_argument('message', help='message for show')
 
 
 def load_plugin():
-    pass
+    config_cli()
